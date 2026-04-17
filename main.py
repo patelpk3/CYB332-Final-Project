1b202df138cca2ab9e09d359ea4eadf1f2fe6a67
Baylen <baylenr@proton.me>
2026-04-17 11:59:36 -0400

    Remove langgraph stuff from main

--- main.py
+++ main.py
@@ -1,28 +1,22 @@
-from langgraph.graph import StateGraph, MessagesState, START, END
 from dotenv import load_dotenv
 
 # local files
 from agents.orchestrator import orchestrator
 from agents.recon import run_recon
+from agents.vul_anal import vul_anal
 
 load_dotenv()
 
 def mock_llm(state: MessagesState):
     return {"messages": [{"role": "ai", "content": "hello world"}]}
 
-graph = StateGraph(MessagesState)
-graph.add_node(mock_llm)
-graph.add_edge(START, "mock_llm")
-graph.add_edge("mock_llm", END)
-graph = graph.compile()
-
-graph.invoke({"messages": [{"role": "user", "content": "hi!"}]})
-
 
 def menu():
+    print("Welcome")
     options = {
         "orchestrator": orchestrator("google.com"),
         "recon": run_recon,
+        "vul_anal": vul_anal,
     }
 
     for i, opt in enumerate(options, 1):
