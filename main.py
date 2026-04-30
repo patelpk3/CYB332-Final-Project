440a70d89f5aa4d14afbc3d60e3a9cf834d8656c
Baylen <baylenr@proton.me>
2026-04-30 17:38:58 -0400

    Minor fix

--- main.py
+++ main.py
@@ -3,20 +3,20 @@ from dotenv import load_dotenv
 # local files
 from agents.orchestrator import orchestrator
 from agents.recon import run_recon
-from agents.vul_anal import vul_anal
+from agents.vul_anal import print_vul_info 
 
 load_dotenv()
 
 def mock_llm(state: MessagesState):
     return {"messages": [{"role": "ai", "content": "hello world"}]}
 
-
 def menu():
     print("Welcome")
     options = {
         "orchestrator": orchestrator("google.com"),
         "recon": run_recon,
-        "vul_anal": vul_anal,
+        "vul_anal": print_vul_info,
+        "report_writer": generate_report
     }
 
     for i, opt in enumerate(options, 1):
